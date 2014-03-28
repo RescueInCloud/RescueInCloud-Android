4e611f3728a4a40a205d3d4b42e7739528eb423e
Ricardo Moisés Champa Bujaico <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-03-29 00:27:15 +0100

    prototipo de menu principal terminado

--- RescueInCloud/src/es/ucm/ric/activities/MenuPrincipalActivity.java
+++ RescueInCloud/src/es/ucm/ric/activities/MenuPrincipalActivity.java
@@ -1,6 +1,8 @@
 package es.ucm.ric.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import es.ucm.ric.R;
 import es.ucm.ric.tools.BaseActivity;
 
@@ -9,6 +11,21 @@ public class MenuPrincipalActivity extends BaseActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+	}
+	
+	public void onClick(View v){
+		
+		switch (v.getId()){
+			case R.id.protocolos:
+				Toast.makeText(this, "Has pulsado protocolos", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.farmacos:
+				Toast.makeText(this, "Has pulsado farmacos", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.notas:
+				Toast.makeText(this, "Has pulsado notas", Toast.LENGTH_SHORT).show();
+				break;
+		}
 	}
 
 }
