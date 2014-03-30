aef823c0a93aecc4650ff16072e4387e33027672
Ricardo Moisés Champa Bujaico <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-03-30 15:04:22 +0200

    borrado el fichero classpath

--- RescueInCloud/src/es/ucm/ric/activities/MenuPrincipalActivity.java
+++ RescueInCloud/src/es/ucm/ric/activities/MenuPrincipalActivity.java
@@ -1,8 +1,10 @@
 package es.ucm.ric.activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import es.ucm.ric.R;
 import es.ucm.ric.tools.BaseActivity;
@@ -12,6 +14,23 @@ public class MenuPrincipalActivity extends BaseActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		
+		Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/Trykker-Regular.ttf");
+        
+		TextView titulo = (TextView)findViewById(R.id.titulo_protocolo);
+		titulo.setTypeface(font);
+		
+		TextView subtitulo = (TextView)findViewById(R.id.subtitulo_protocolo);
+		subtitulo.setTypeface(font);
+		
+//		Typeface fontRoboto = Typeface.createFromAsset(this.getAssets(), "fonts/Roboto-Medium.ttf");
+//        
+//		TextView tituloF = (TextView)findViewById(R.id.titulo_farmaco);
+//		tituloF.setTypeface(fontRoboto);
+//		
+//		TextView subtituloF = (TextView)findViewById(R.id.subtitulo_farmaco);
+//		subtituloF.setTypeface(fontRoboto);
 	}
 	
 	public void onClick(View v){
