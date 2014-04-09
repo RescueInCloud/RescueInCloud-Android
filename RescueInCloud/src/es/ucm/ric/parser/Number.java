b8a127798ab60a20cb17e6240fc1d6d94c4a5102
mi Username <cld.car.ma@gmail.com>
2014-04-09 12:06:59 +0200

    corrección del error del parser
    he comprobado que funcione tanto con la unidad con espacio o sin el y
    tanto en el ultimo lugar de la frase como en otro

--- RescueInCloud/src/es/ucm/ric/parser/Number.java
+++ RescueInCloud/src/es/ucm/ric/parser/Number.java
@@ -45,20 +45,22 @@ public class Number extends TextInterpreter{
             if (isNumber(w[i])){
             	 encontrado=true;
             	 command = new ArrayList<String>();
-            	 if(!isUnit(w[i+1])){
-	                command.add("Number");//0->tipo
-	                command.add(w[0].replace("id", ""));//1->id
-	                command.add(w[i]);//2->valor
-	                command.add(encontrarRelacionCantidad(cadena,w[i]));//3->rC
-	                command.add(cadena);
-            	 }
-            	 else{
-            		 command.add("NumberUnit"); //0->nombre
-                     command.add(w[0].replace("id", "")); //1->id
-                     command.add(w[i]); //2->valor
-                     command.add(w[i+1]); //3->unidad
-                     command.add(this.encontrarRelacionCantidad(cadena,w[i]));//4->relacionCant
-                     command.add(texto); //4->texto
+            	 if (i+1<w.length){
+	            	 if(!isUnit(w[i+1])){
+		                command.add("Number");//0->tipo
+		                command.add(w[0].replace("id", ""));//1->id
+		                command.add(w[i]);//2->valor
+		                command.add(encontrarRelacionCantidad(cadena,w[i]));//3->rC
+		                command.add(cadena);
+	            	 }
+	            	 else{
+	            		 command.add("NumberUnit"); //0->nombre
+	                     command.add(w[0].replace("id", "")); //1->id
+	                     command.add(w[i]); //2->valor
+	                     command.add(w[i+1]); //3->unidad
+	                     command.add(this.encontrarRelacionCantidad(cadena,w[i]));//4->relacionCant
+	                     command.add(texto); //4->texto
+	            	 }
             	 }
             }
             i++;
