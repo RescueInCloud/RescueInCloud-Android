dbdeeaf164f78a417e23b8e0e571402ac25e6c2d
Ricardo <ricardochampa@gmail.com>
2014-05-05 01:02:07 +0200

    Detalle de farmaco completo

--- RescueInCloud/src/es/ucm/ric/activities/fragments/details/FragmentDetalleFarmaco.java
+++ RescueInCloud/src/es/ucm/ric/activities/fragments/details/FragmentDetalleFarmaco.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import es.ucm.ric.R;
 import es.ucm.ric.dao.FarmacoDAO;
 import es.ucm.ric.model.Farmaco;
 
@@ -27,5 +29,20 @@ public class FragmentDetalleFarmaco extends FragmentDetalle{
 		
 		FarmacoDAO dao = new FarmacoDAO();
 		Farmaco f = dao.get(id);
+		
+		TextView nombre = (TextView) getActivity().findViewById(R.id.nombre_farmaco);
+		TextView fabricante = (TextView) getActivity().findViewById(R.id.nombre_fabricante);
+		TextView presentacion = (TextView) getActivity().findViewById(R.id.presentacion_farmaco);
+		TextView tipo_presentacion = (TextView) getActivity().findViewById(R.id.tipo_presentacion);
+		TextView descripcion = (TextView) getActivity().findViewById(R.id.descripcion);
+		
+		
+		nombre.setText(f.getNombre_farmaco());
+		fabricante.setText(f.getNombre_fabricante());
+		presentacion.setText(f.getPresentacion_farmaco());
+		tipo_presentacion.setText(f.getTipo_presentacion());
+		descripcion.setText(f.getDescripcion_farmaco());
 	}
+	
+	
 }
